508cc51507aabbe33b350fcfdfca76d446181ad5
GallaFrancesco <francesco.galla3@gmail.com>
2020-11-13 10:46:47 +0100

    util: added extra checks to ensure passwords are not serialized

--- src/main/java/it/mynaproject/gestprod/util/ClassSerializer.java
+++ src/main/java/it/mynaproject/gestprod/util/ClassSerializer.java
@@ -10,11 +10,15 @@ public class ClassSerializer {
 		this.obj = obj;
 	}
 	
+	// WARNING: could serialize important user data (password)
+	// if cases added as mitigation 
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append(obj.getClass().getName());
 		builder.append(" [");
 		for(Field f: obj.getClass().getDeclaredFields()) {
+			if(f.getName().equals("password") ||
+			   f.getName().equals("oldPassword")) continue;
 			builder.append(f.getName());
 			builder.append(" = ");
 			try {
