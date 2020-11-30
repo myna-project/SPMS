9ef206cf25ddb8be677c48ae934cc3e85506b302
GallaFrancesco <me@fragal.eu>
2020-11-30 17:25:04 +0100

    service: FIXUP AdditiveProductionOrder removed unnecessary checks and todo

--- src/main/java/it/mynaproject/gestprod/service/impl/AdditiveProductionOrderServiceImpl.java
+++ src/main/java/it/mynaproject/gestprod/service/impl/AdditiveProductionOrderServiceImpl.java
@@ -25,7 +25,6 @@ public class AdditiveProductionOrderServiceImpl implements AdditiveProductionOrd
 	@Autowired
 	private AdditiveProductionOrderDao additiveProductionOrderDao;
 
-	// TODO su tutti ricontrollare codici errore
 	@Transactional(readOnly = true)
 	@Override
 	public AdditiveProductionOrder getAdditiveProductionOrder(Integer id) {
@@ -59,9 +58,6 @@ public class AdditiveProductionOrderServiceImpl implements AdditiveProductionOrd
 
 		log.info("Creating new additiveProductionOrder: {}", input.toString());
 
-		if(this.additiveProductionOrderDao.getAdditiveProductionOrder(input.getId()) != null) {
-			throw new ConflictException(2001, "AdditiveProductionOrder already registered with id: " + input.getId());
-		}
 		AdditiveProductionOrder additiveProductionOrder = new AdditiveProductionOrder();
 		additiveProductionOrder.populateAdditiveProductionOrderFromInput(input);
 
@@ -82,11 +78,6 @@ public class AdditiveProductionOrderServiceImpl implements AdditiveProductionOrd
 
 		log.info("Updating additiveProductionOrder with id: {}", id);
 
-		for(AdditiveProductionOrder e: this.additiveProductionOrderDao.getAdditiveProductionOrders()) { // TODO fix comparison over ID
-			if(e.getAdditive().getId().equals(input.getAdditive_id()) && e.getProductionOrder().getId().equals(input.getProduction_order_code_id())) {
-				throw new ConflictException(2001, "AdditiveProductionOrder (additive id: " + input.getAdditive_id() + ", PO id: " + input.getProduction_order_code_id() + ") already registered.");
-			}
-		}
 		AdditiveProductionOrder additiveProductionOrder = this.getAdditiveProductionOrder(id);
 		additiveProductionOrder.populateAdditiveProductionOrderFromInput(input);
 
@@ -106,10 +97,6 @@ public class AdditiveProductionOrderServiceImpl implements AdditiveProductionOrd
 			throw new NotFoundException(404, "AdditiveProductionOrder " + id + " not found");
 		}
 		
-		Date now = new Date();
-		ProductionOrder po = c.getProductionOrder(); 
-		if(now.after(po.getDelivery_date()))
-			throw new ConflictException(2101, "Cannot delete additiveProductionOrder: " + id + ", production order already delivered.");
 		this.additiveProductionOrderDao.delete(c);
 	}
 }
