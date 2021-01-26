972dfa2abf861415332acaa6dab05ea9b44f0361
paologrosso <paolo.grosso@myna-project.org>
2021-01-26 09:46:19 +0100

    Edit DB script creation

--- ddl/creation/gestprod_0.0.1.sql
+++ ddl/creation/gestprod_0.0.1.sql
@@ -164,7 +164,10 @@ ALTER TABLE system_preparation_phases ADD CONSTRAINT user_id__users_fkey FOREIGN
 
 CREATE TABLE working_phases (
     id serial,
-    production_order_id integer NOT NULL
+    production_order_id integer NOT NULL,
+    user_id integer NOT NULL,
+    start_time timestamp with time zone,
+    end_time timestamp with time zone
 );
 
 ALTER TABLE working_phases OWNER TO gestprod;
@@ -172,21 +175,7 @@ ALTER TABLE working_phases OWNER TO gestprod;
 ALTER TABLE working_phases ADD CONSTRAINT working_phases_id_pkey PRIMARY KEY (id);
 
 ALTER TABLE working_phases ADD CONSTRAINT production_order_id__production_orders__fkey FOREIGN KEY (production_order_id) REFERENCES production_orders(id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
-
-CREATE TABLE working_phases_users (
-	id serial,
-    working_phase_id integer NOT NULL,
-    user_id integer NOT NULL,
-    start_time timestamp with time zone,
-    end_time timestamp with time zone
-);
-
-ALTER TABLE working_phases_users OWNER TO gestprod;
-
-ALTER TABLE working_phases_users ADD CONSTRAINT working_phases_pkey PRIMARY KEY (id);
-
-ALTER TABLE working_phases_users ADD CONSTRAINT working_phase_id__working_phases__fkey FOREIGN KEY (working_phase_id) REFERENCES working_phases(id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE working_phases_users ADD CONSTRAINT user_id__users_fkey FOREIGN KEY (user_id) REFERENCES users(id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE working_phases ADD CONSTRAINT user_id__users_fkey FOREIGN KEY (user_id) REFERENCES users(id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE TABLE cleaning_phases (
     id serial,
@@ -209,8 +198,8 @@ CREATE TABLE validation_phases (
     user_id integer NOT NULL,
     start_time timestamp with time zone,
     end_time timestamp with time zone,
-    humidity_finished_product double precision NOT NULL,
-    density_finished_product double precision NOT NULL,
+    humidity_finished_product double precision,
+    density_finished_product double precision,
     packaging_state text,
     sieve_quantity double precision,
     chimney_quantity double precision,
@@ -232,7 +221,6 @@ ALTER TABLE validation_phases ADD CONSTRAINT user_id__users_fkey FOREIGN KEY (us
 CREATE TABLE working_phases_measurements (
 	id serial,
 	working_phase_id integer NOT NULL,
-	user_id integer NOT NULL,
 	time timestamp with time zone NOT NULL,
 	finished_product_quantity double precision
 );
@@ -241,4 +229,3 @@ ALTER TABLE working_phases_measurements OWNER TO gestprod;
 
 ALTER TABLE working_phases_measurements ADD CONSTRAINT working_phases_measurements_id_pkey PRIMARY KEY (id);
 ALTER TABLE working_phases_measurements ADD CONSTRAINT working_phase_id__working_phases__fkey FOREIGN KEY (working_phase_id) REFERENCES working_phases(id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;    
-ALTER TABLE working_phases_measurements ADD CONSTRAINT user_id__users_fkey FOREIGN KEY (user_id) REFERENCES users(id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION;
